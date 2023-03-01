76ab0b99eba7cabe429efe6d85f22784d7a0d544
Adrien Poupard <adrien@smartb.city>
2023-03-01 09:16:07 +0100

    DSL - Make Referentiel Framework open

--- cccev-dsl/dsl-model/src/commonMain/kotlin/cccev/dsl/model/ReferenceFramework.kt
+++ cccev-dsl/dsl-model/src/commonMain/kotlin/cccev/dsl/model/ReferenceFramework.kt
@@ -1,5 +1,26 @@
 package cccev.dsl.model
 
-data class ReferenceFramework(
+open class ReferenceFramework(
 	val identifier: String,
-)
+) {
+	override fun toString(): String {
+		return "ReferenceFramework(identifier='$identifier')"
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other == null || this::class != other::class) return false
+
+		other as ReferenceFramework
+
+		if (identifier != other.identifier) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		return identifier.hashCode()
+	}
+
+
+}
